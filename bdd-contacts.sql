ea756a40130a2b3d6c2c81a0fa632b33f9291557
Duff <dilan3268@gmail.com>
2023-11-23 09:51:19 +0100

    Créer la table contacts

--- bdd-contacts.sql
+++ bdd-contacts.sql
@@ -8,3 +8,16 @@ CREATE TABLE IF NOT EXISTS pays (
     iso_2 VARCHAR(2) NOT NULL,
     nationalite VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(70) NOT NULL,
+    prenom VARCHAR(70) NOT NULL,
+    date_de_naissance DATE NOT NULL,
+    sexe VARCHAR(10) NOT NULL,
+    adresse TEXT NOT NULL,
+    cp VARCHAR(10) NOT NULL,
+    ville VARCHAR(70) NOT NULL,
+    pays_iso_3 VARCHAR(3),
+    FOREIGN KEY (pays_iso_3) REFERENCES pays(iso_3) ON DELETE SET NULL
+);
